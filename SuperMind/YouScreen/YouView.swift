dcb9c72050e8469905359a98157ddca34e56de05
Aryan <aryankhator.ak@gmail.com>
2025-02-27 22:58:26 +0530

    insight view added

--- SuperMind/YouScreen/YouView.swift
+++ SuperMind/YouScreen/YouView.swift
@@ -104,7 +104,31 @@ struct YouView: View {
                     MyLibraryView()
                         .padding(.top, 40)
                 }else {
-                    
+                    HStack {
+                        VStack {
+                            Image(systemName: "triangle.fill")
+                            Image(systemName: "triangle.fill")
+                        }
+                        .foregroundStyle(LinearGradient(colors: [.gradeint6, .white], startPoint: .top, endPoint: .bottom))
+                        Text("30%")
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundStyle(.gradeint6)
+                        Rectangle()
+                            .frame(maxHeight: .infinity)
+                            .frame(width: 2)
+                            .foregroundStyle(.gradeint6)
+                            .padding(12)
+                        Text("Your mindfulness practice\nincreased by 30% this week")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(height: 84)
+                    .padding(.horizontal, 12)
+                    .background(LinearGradient(colors: [.gradient4, .gradient5], startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(20)
+                    .padding(EdgeInsets(top: 40, leading: 16, bottom: 16, trailing: 0))
+                    StatisticsView(stats: viewModel.stats)
                 }
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Made Mindfully in 🇮🇳")
@@ -159,3 +183,68 @@ struct MyLibraryView: View {
     }
 }
 
+
+struct StatisticsView: View {
+    let stats: [StatItem]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("My statistics:")
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(.white)
+                .padding(.horizontal)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 16) {
+                    ForEach(stats, id: \.id) { stat in
+                        StatCardView(stat: stat)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+        .padding(.top)
+    }
+}
+
+struct StatCardView: View {
+    let stat: StatItem
+    
+    var body: some View {
+        VStack {
+            Text(stat.title)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(stat.color)
+            
+            Image(stat.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 90, height: 90)
+            Rectangle()
+                .frame(width: 120, height: 1)
+                .foregroundStyle(.gray)
+            HStack {
+                VStack(spacing: 8) {
+                    Text(stat.value1)
+                        .font(.system(size: 20, weight: .medium))
+                        .foregroundColor(stat.color)
+                    Text(stat.unit1)
+                        .foregroundColor(.white)
+                        .font(.system(size: 14, weight: .regular))
+                }
+                VStack(spacing: 8) {
+                    Text(stat.value2)
+                        .font(.system(size: 20, weight: .medium))
+                        .foregroundColor(stat.color)
+                    Text(stat.unit2)
+                        .foregroundColor(.white)
+                        .font(.system(size: 14, weight: .regular))
+                }
+            }
+        }
+        .frame(width: 128, height: 200)
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(16)
+        .background(RoundedRectangle(cornerRadius: 16).stroke(.gray, lineWidth: 1))
+        .padding(1)
+    }
+}
--- SuperMind/YouScreen/YouViewModel.swift
+++ SuperMind/YouScreen/YouViewModel.swift
@@ -12,7 +12,8 @@ import SwiftUI
 class YouViewModel: ObservableObject {
     @Published var recent: [Meditation] = []
     @Published var isMyLibrarySelected = true
-
+    @Published var stats: [StatItem] = []
+        
     init() {
         fetchDummyData()
     }
@@ -23,6 +24,14 @@ class YouViewModel: ObservableObject {
             Meditation(title: "Freedom Series", subtitle: "Meditation Series", imageName: "sampleImage2", author: "Akhil Aryan", duration: "5 Mins"),
             Meditation(title: "Freedom Series", subtitle: "Meditation - 10 Mins", imageName: "sampleImage1", author: "Akhil Aryan", duration: "10 Mins"),
             Meditation(title: "Freedom Series", subtitle: "Meditation Series", imageName: "sampleImage2", author: "Akhil Aryan", duration: "5 Mins")
+        ]
+        stats = [
+            StatItem(title: "Meditation", image: "meditation", color: .green, value1: "200", unit1: "Sessions", value2: "700", unit2: "XP"),
+            StatItem(title: "Workout", image: "workout", color: .yellow, value1: "200", unit1: "Minutes", value2: "700", unit2: "XP"),
+            StatItem(title: "Journal", image: "journal", color: .pink, value1: "200", unit1: "Entries", value2: "700", unit2: "XP"),
+            StatItem(title: "Meditation", image: "meditation", color: .green, value1: "200", unit1: "Sessions", value2: "700", unit2: "XP"),
+            StatItem(title: "Workout", image: "workout", color: .yellow, value1: "200", unit1: "Minutes", value2: "700", unit2: "XP"),
+            StatItem(title: "Journal", image: "journal", color: .pink, value1: "200", unit1: "Entries", value2: "700", unit2: "XP")
         ]
     }
 }
