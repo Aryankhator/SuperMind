574b73010f332e5be077a83847055806c23d3543
Aryan <aryankhator.ak@gmail.com>
2025-02-27 11:34:48 +0530

    Tabview added

---
+++ SuperMind/YouScreen/YouView.swift
@@ -0,0 +1,20 @@
+//
+//
+// YouView.swift
+// SuperMind
+//
+// Created by Nand on 27/02/25
+//
+        
+
+import SwiftUI
+
+struct YouView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouView()
+}
