c886e1bb7ec7a29cab8e0bd658ed23d39cdf20cf
Aryan <aryankhator.ak@gmail.com>
2025-02-27 10:49:16 +0530

    intial commit

---
+++ SuperMind/SuperMindApp.swift
@@ -0,0 +1,19 @@
+//
+//
+// SuperMindApp.swift
+// SuperMind
+//
+// Created by Aryan on 27/02/25
+//
+        
+
+import SwiftUI
+
+@main
+struct SuperMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
